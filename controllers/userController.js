cb8df6ca288df712a103ee4735fee5e1ed30d645
= <lukejmullins@gmail.com>
2026-04-02 15:45:42 -0400

    Add user validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,34 +2,39 @@ const service = require("../services/userService");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const secret = process.env.SECRET;
+const validateUser = require("../validation/validateUser");
+const { validationResult, matchedData } = require("express-validator");
 
-const postUser = async (req, res) => {
-  try {
-    const { username, password } = req.body;
+const postUser = [
+  validateUser,
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-    if (!username || !password) {
-      return res
-        .status(400)
-        .json({ message: "Username and password are required" });
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: errors.array()[0].msg });
+      }
+
+      const { username, password } = matchedData(req);
+
+      const userExists = await service.getUser(username);
+
+      if (userExists) {
+        return res
+          .status(400)
+          .json({ message: "Username already registered. Try again." });
+      }
+
+      const hashPassword = await bcrypt.hash(password, 10);
+
+      await service.postUser(username, hashPassword);
+
+      return res.status(201).json({ username, password });
+    } catch {
+      return res.status(500).json({ message: "Failed to create user" });
     }
-
-    const userExists = await service.getUser(username);
-
-    if (userExists) {
-      return res
-        .status(400)
-        .json({ message: "Username already registered. Try again." });
-    }
-
-    const hashPassword = await bcrypt.hash(password, 10);
-
-    await service.postUser(username, hashPassword);
-
-    return res.status(201).json({ username, password });
-  } catch {
-    return res.status(500).json({ message: "Failed to create user" });
-  }
-};
+  },
+];
 
 const login = async (req, res) => {
   try {
---
+++ validation/validateUser.js
@@ -0,0 +1,29 @@
+const { body } = require("express-validator");
+
+const spaceErr = " must have no spaces.";
+const userLengthErr =
+  " must be more than 3 characters and less than 16 characters.";
+const passwordLengthErr = " must be at least 8 characters long.";
+const mismatchErr =
+  " must contain at least one uppercase letter, one lowercase letter, one number, and one symbol.";
+const passwordErr = "Passwords don't match.";
+
+const validateUser = [
+  body("username")
+    .custom((value) => !/\s/.test(value))
+    .withMessage(`Username${spaceErr}`)
+    .isLength({ min: 4, max: 15 })
+    .withMessage(`Username${userLengthErr}`),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage(`Password${passwordLengthErr}`)
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).*$/)
+    .withMessage(`Password${mismatchErr}`),
+  body("confirmPassword")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage(`${passwordErr}`),
+];
+
+module.exports = validateUser;
